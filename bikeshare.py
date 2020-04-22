1bca7ba4fa3f1c6c150223ff7009242b76a47567
ole dening <ole.dening@gmail.com>
2020-04-22 11:13:35 +0200

    explained more descriptively the CITY_DATA

--- bikeshare.py
+++ bikeshare.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import json
 
+# These are the examined cities and their respective data files
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
